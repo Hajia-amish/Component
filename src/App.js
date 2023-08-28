30449efe975541fba0d73f84799558220a763524
Amish <hajiaamish533@gmail.com>
2023-08-28 12:29:05 -0700

    seem

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+let App = () => 
+  return (
+    
+  ) 
+    <div className="App" >
+
+      <h1>"Codetrain is awesome, react works!!!"</h1>
+    </div>
+
+
+export default App;
